e61314925d8332d7de5b933565f72ea3ee589a61
Bayarkhuu <bayarkhuu.srs@yahoo.com>
2021-02-17 19:18:02 +0800

    main design

---
+++ app.js
@@ -0,0 +1,19 @@
+function navigateToHome() {
+    globalThis.location.href = "index.html";
+}
+
+function navigateToMe() {
+    globalThis.location.href = "Me.html";
+}
+
+function navigateToSkill() {
+    globalThis.location.href = "Skill.html";
+}
+
+function navigateToContact() {
+    globalThis.location.href = "Contact.html";
+}
+
+function navigateToFaceBook() {
+    globalThis.open("https://www.facebook.com/SerisEyes");
+}
